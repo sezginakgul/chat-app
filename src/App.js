3d79a5b448c4640fa00cd4403b6253f8a924c003
sezgin <sezgin.akgull@gmail.com>
2023-01-23 23:47:42 +0300

    Added project components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Home />
     </div>
   );
 }
