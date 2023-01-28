cf7f4eda7ca4b5f244858d8684a6298991f31918
sezgin <sezgin.akgull@gmail.com>
2023-01-28 23:23:35 +0300

    add input components

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Img from "../images/img.png";
+import Attach from "../images/attach.png";
 
 const Input = () => {
-  return <div className="input">Input</div>;
+  return (
+    <div className="input">
+      <input type="text" placeholder="Type something..." />
+      <div className="send">
+        <img className="send-img" src={Attach} alt="" />
+        <input type="file" name="" style={{ display: "none" }} id="file" />
+        <label htmlFor="file">
+          <img className="send-img" src={Img} alt="" />
+        </label>
+        <button className="send-btn">Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Input;
