58a501aedc89358b465e731e265197befb53018a
sezgin <sezgin.akgull@gmail.com>
2023-01-29 00:05:05 +0300

    adding and styling message components

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      <div className="message-info">
+        <img
+          src="https://images.pexels.com/photos/14829062/pexels-photo-14829062.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <span>Just now</span>
+      </div>
+      <div className="message-content">
+        <p>Hello</p>
+        <img
+          src="https://images.pexels.com/photos/14829062/pexels-photo-14829062.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
